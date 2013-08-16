0d4ec54800a5081e6a4c08a8a6ddbc43be6d2df4
fabien0102 <fabien0102@planee.fr>
2013-08-16 13:04:41 +0200

    Clean example file

--- examples/index.js
+++ examples/index.js
@@ -23,34 +23,5 @@ gitGraph.commit();
 gitGraph.commit();
 gitGraph.commit(null, dev);
 master.checkout();
-dev.merge();;
-test.merge();
-
-
-/*var master = new Branch({
-  context: context,
-  name: "master",
-  origin: canvas.height - 20,
-});
-
-var dev = new Branch({
-  context: context,
-  name: "dev",
-  origin: canvas.height - 120,
-  //color: "green",
-  size: 40,
-  parent: master
-});
-master.checkout();
 dev.merge();
-
-var test = new Branch({
-  context: context,
-  name: "test",
-  origin: canvas.height - 120,
-  //color: "blue",
-  size: 200,
-  parent: master
-});
-master.checkout();
-test.merge();*/
+test.merge();
