98a509615bbb9b6463299f70e7b3d752fa3a444d
fabien0102 <fabien0102@planee.fr>
2013-08-25 18:22:50 +0200

    New example template

--- examples/index.js
+++ examples/index.js
@@ -1,27 +1,46 @@
-var gitGraph = new GitGraph({
-  template: new Template({
-    branch: {
-      color: '#000000',
-      lineWidth: 4,
-      margin: 50,
-      mergeStyle: 'straight'
+var mytemplate = new Template({
+  branch: {
+    color: '#000000',
+    lineWidth: 4,
+    margin: 50,
+    mergeStyle: 'straight'
+  },
+  commit: {
+    spacing: 60,
+    dot: {
+      size: 12,
+      strokeColor: '#000000',
+      strokeWidth: 7
     },
-    commit: {
-      spacing: 60,
-      dot: {
-        size: 12,
-        strokeColor: '#000000',
-        strokeWidth: 7
-      },
-      message: {
-        color: 'black'
-      }
-    },
-    arrow: {
-      height: 16,
-      width: 13
+    message: {
+      color: 'black'
     }
-  })
+  },
+  arrow: {
+    height: 16,
+    width: 13
+  }
+});
+
+var mytemplate2 = new Template({
+  colors: ['#979797', '#008fb5', 'f1c109'],
+  branch: {
+    lineWidth: 10,
+    margin: 50
+  },
+  commit: {
+    spacing: 80,
+    dot: {
+      size: 14
+    },
+    message: {
+      font: 'normal 14pt Arial',
+    }
+  },
+});
+
+var gitGraph = new GitGraph({
+  template: mytemplate2
 });
 
 var master = gitGraph.branch({
@@ -35,7 +54,13 @@ var dev = gitGraph.branch({
   name: 'dev',
 });
 gitGraph.commit('Youhou \\o/');
-gitGraph.commit();
+gitGraph.commit({
+  dotColor: 'white',
+  dotSize: 10,
+  dotStrokeWidth: 10,
+  message: "Alors c'est qui le papa ?",
+  author: "Me <me@planee.fr>"
+});
 gitGraph.commit();
 
 //master.checkout();
@@ -43,7 +68,7 @@ var test = gitGraph.branch({
   name: 'test',
 });
 gitGraph.commit({
-message: 'test'
+  message: 'test'
 });
 gitGraph.commit();
 dev.commit(); // Commit on 'dev' Branch
