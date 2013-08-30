50d2be6c62c17ce21283e6d1487ec71d93dedca1
fabien0102 <fabien0102@planee.fr>
2013-08-30 20:25:41 +0200

    Clean & comment Example

--- examples/index.js
+++ examples/index.js
@@ -2,18 +2,16 @@ var gitGraph = new GitGraph({
   template: 'blackarrow' // 'blackarrow' or 'metro' or Template Object
 });
 
-var master = gitGraph.branch('master');
-gitGraph.commit({
-  message:'Initial commit' ,
-  color: 'pink'
-}); // Commit on HEAD Branch
-gitGraph.commit();
-gitGraph.commit();
+var master = gitGraph.branch('master'); // Create branch named 'master'
+gitGraph.commit('Initial commit') // Commit on HEAD Branch (here this is master)
+.commit('My second commit')
+.commit('Add awesome feature');
 
-var dev = gitGraph.branch({
-  name: 'dev',
-});
-gitGraph.commit('Youhou \\o/');
+var dev = gitGraph.branch('dev'); // Create branch named 'dev' (a HEAD fork)
+dev.commit('Youhou \\o/'); // Commit on dev
+master.commit("I'm the master !"); // Commit on master
+
+// Advanced commit method with style and specific author (HEAD)
 gitGraph.commit({
   dotColor: 'white',
   dotSize: 10,
@@ -21,22 +19,23 @@ gitGraph.commit({
   message: "Alors c'est qui le papa ?",
   author: "Me <me@planee.fr>"
 });
-gitGraph.commit();
 
-//master.checkout();
-var test = gitGraph.branch({
-  name: 'test',
-});
+//master.checkout(); // Checkout on master branch for create 'test' since master
+var test = gitGraph.branch('test');
 gitGraph.commit({
   message: 'test'
 });
 gitGraph.commit();
-dev.commit().commit(); // Commit on 'dev' Branch
-master.checkout();
-gitGraph.author = 'Fabien0102 <fabien0102@planee.fr>'; // Change author
-master.commit();
-dev.merge();
-test.commit();
+dev.commit().commit(); // 2 default Commit on 'dev'
+
+gitGraph.author = 'Fabien0102 <fabien0102@planee.fr>'; // Change default author
+master.commit(); // Default commit on master (you can see the new author)
+
+master.checkout(); // Checkout on master branch
+dev.merge(); // Merge dev into HEAD (master) (automatic merge commit)
+test.commit('Final commit');
 test.merge(null, false); // Merge into HEAD without merge commit
+
 test.commit('error'); // Error: You can't commit on merged branch
-gitGraph.render();
+
+gitGraph.render(); // Render gitGraph
