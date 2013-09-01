0e791e4d4b10fdba496c66394e6a23b838093fa3
Espeon <espeon@minet.net>
2013-09-01 12:36:16 +0200

    Add JSHint grunt task to lint the JavaScript code
    This will compare our code against best practices defined by JSHint
    Configuration for the linter is in .jshintrc file
    The `grunt jshint` task allows you to lint the code now

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,16 @@ module.exports = function ( grunt ) {
   // Initialize tasks for Grunt
   grunt.initConfig( {
 
+    // The `jshint` task lint all the JavaScript code against best practices.
+    // Warnings are here to help us improve code and follow best standards.
+    jshint: {
+      src: [ "src/*.js" ],
+      options: {
+        force: true,
+        jshintrc: ".jshintrc"
+      }
+    }
+
   } );
 
   // Loads grunt tasks from `package.json`
