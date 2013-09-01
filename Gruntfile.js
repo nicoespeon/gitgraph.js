204f6d4da893f98550125d528f0b329dcfe39df6
Espeon <espeon@minet.net>
2013-09-01 12:23:45 +0200

    Initialize the Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+// This file defines grunt tasks used by [Grunt.js](http://gruntjs.com/sample-gruntfile)
+module.exports = function ( grunt ) {
+
+  // Initialize tasks for Grunt
+  grunt.initConfig( {
+
+  } );
+
+};
