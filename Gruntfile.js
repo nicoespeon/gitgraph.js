ca3cbdae79dd501265f2186a61b6a9741435e96a
Espeon <espeon@minet.net>
2013-09-01 13:54:03 +0200

    Add JSDoc grunt task
    Generate documentation parsing code comments
    Create a diet task which is not versioned, and a release one to launch for official documentation generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,22 @@ module.exports = function ( grunt ) {
   // Initialize tasks for Grunt
   grunt.initConfig( {
 
+    // The `jsdoc` task will produce the code documentation for the whole project.
+    jsdoc: {
+      dist: {
+        src: [ "src/*.js" ],
+        options: {
+          destination: "dist/docs"
+        }
+      },
+      release: {
+        src: [ "src/*.js" ],
+        options: {
+          destination: "docs"
+        }
+      }
+    },
+
     // The `jshint` task lint all the JavaScript code against best practices.
     // Warnings are here to help us improve code and follow best standards.
     jshint: {
