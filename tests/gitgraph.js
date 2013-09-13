2e141386ca58b5d77e8380109d136a3e662568d6
fabien0102 <fabien0102@planee.fr>
2013-09-13 15:13:51 +0200

    Add some basics tests

--- tests/gitgraph.js
+++ tests/gitgraph.js
@@ -1,7 +1,112 @@
-describe("Gitgraph", function() {
+// Scenario
+var gitGraph = new GitGraph({
+  canvas: document.createElement("canvas"),
+  testMode: true,
+  template: "metro"
+});
 
-  it("should have a basic test", function() {
+var master = gitGraph.branch("master").commit().commit();
+var develop = gitGraph.branch("develop").commit();
+master.commit("message");
+gitGraph.commit("plop"); // HEAD = develop
 
+// Tests
+describe("Gitgraph", function () {
+  it("should hide message in compact mode", function () {
+    var gitGraph = new GitGraph({
+      canvas: document.createElement("canvas"),
+      testMode: true,
+      mode: "compact"
+    });
+
+    expect(gitGraph.template.commit.message.display)
+      .toEqual(false);
   });
 
+  it("should load the right template", function () {
+    expect(gitGraph.template)
+      .toEqual(new Template().get("metro"));
+  });
+  
+  it("should load the default template", function () {
+    var gitGraph = new GitGraph({
+      canvas: document.createElement("canvas"),
+      testMode: true,
+      template: "wrongInput"
+    });
+    expect(gitGraph.template)
+      .toEqual(new Template());
+  });
+});
+
+
+describe("Branch", function () {
+  it("shoud have a name", function () {
+    expect(gitGraph.branchs[0].name)
+      .toEqual("master");
+  });
+
+  it("should be HEAD on develop", function () {
+    expect(gitGraph.HEAD)
+      .toEqual(develop);
+  });
+  
+  it("should have the first color of template theme", function () {
+    expect(master.color)
+      .toEqual(gitGraph.template.colors[0]);
+  });
+  
+  it("should have the color of branch template", function () {
+    var gitGraph = new GitGraph({
+      canvas: document.createElement("canvas"),
+      testMode: true,
+      template: "blackarrow"
+    });
+    var master = gitGraph.branch("master");
+    
+    expect(master.color)
+      .toEqual(gitGraph.template.branch.color);
+  });
+  
+  it("should have the right column number", function () {
+    expect(master.column)
+      .toEqual(0);    
+    expect(develop.column)
+      .toEqual(1);    
+  });
+  
+  it("should have the right commits count", function () {
+    expect(master.commits.length)
+      .toEqual(3);
+    expect(develop.commits.length)
+      .toEqual(2);
+  });
+});
+
+describe("Commit", function () {
+  it("should have the right message", function () {
+    expect(master.commits[2].message)
+      .toEqual("message");
+  });
+  
+  it("should have a pretty color", function () {
+    expect(master.commits[2].dotColor)
+      .toEqual(gitGraph.template.colors[0]);
+    expect(master.commits[2].messageColor)
+      .toEqual(gitGraph.template.colors[0]);
+  });
+  
+  it("should have the right position", function () {
+    // Commit on master
+    expect(master.commits[2].x)
+      .toEqual(0);
+    expect(master.commits[2].y)
+      .toEqual(240);
+    
+    // Commit on develop
+    expect(develop.commits[1].x)
+      .toEqual(50);
+    expect(develop.commits[1].y)
+      .toEqual(320);
+  });
 });
