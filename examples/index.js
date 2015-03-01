0c44550982b212c557abeaf2fb967c9b2f8a7244
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2015-03-01 11:29:04 +0100

    Add last PRs enhancements into examples

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,33 @@
 /***********************
+ *  CUSTOM TEMPLATES   *
+ ***********************/
+
+var myTemplateConfig = {
+  colors: [ "#F00", "#0F0", "#00F" ], // branches colors, 1 per column
+  branch: {
+    lineWidth: 8,
+    spacingX: 50
+  },
+  commit: {
+    spacingY: -80,
+    dot: {
+      size: 12
+    },
+    message: {
+      displayAuthor: true,
+      displayHash: false,
+      font: "normal 12pt Arial"
+    }
+  }
+};
+var myTemplate = new GitGraphTemplate( myTemplateConfig );
+
+/***********************
  *    INITIALIZATION   *
  ***********************/
 
 var config = {
-  template: "metro"       // could be: "blackarrow" or "metro" or Template Object
+  template: "metro"       // could be: "blackarrow" or "metro" or myTemplate (custom Template object)
   //, mode: "compact"     // special compact mode : hide messages & compact graph
 };
 var gitGraph = new GitGraph( config );
@@ -33,6 +57,8 @@ var commitConfig = {
   dotColor: "white",
   dotSize: 10,
   dotStrokeWidth: 10,
+  messageHashDisplay: false,
+  messageAuthorDisplay: true,
   message: "Alors c'est qui le papa ?",
   author: "Me <me@planee.fr>"
 };
