b62457d81fd9db9bb7d7b563efb5e7f7164e3038
Jon Ringle <jringle@gridpoint.com>
2015-07-14 02:32:35 -0400

    Update gitflowsupport.js example to include branch labels
    You can see this example at http://code.ringle.org/gitflowsupport.html
    
    Signed-off-by: Jon Ringle <jringle@gridpoint.com>

--- examples/gitflowsupport.js
+++ examples/gitflowsupport.js
@@ -2,21 +2,27 @@ var graphConfig = new GitGraph.Template({
     branch: {
         color: "#000000",
         lineWidth: 3,
-        spacingX: 50,
-        mergeStyle: "straight"
+        spacingX: 60,
+        mergeStyle: "straight",
+        showLabel: true,
+        labelFont: "normal 10pt Arial"
     },
     commit: {
         spacingY: -30,
         dot: {
-            size: 6,
+            size: 8,
             strokeColor: "#000000",
             strokeWidth: 4
         },
+        tag: {
+            font: "normal 10pt Arial",
+            color: "yellow"
+        },
         message: {
             color: "black",
-            font: "normal 14pt Arial",
+            font: "normal 12pt Arial",
             displayAuthor: false,
-            displayBranch: true,
+            displayBranch: false,
             displayHash: false,
         }
     },
@@ -56,7 +62,7 @@ var gitgraph = new GitGraph(config);
 
 var master = gitgraph.branch({name:"master", column:masterCol});
 var develop = gitgraph.branch({parentBranch:master, name: "develop", column:developCol});
-master.commit(" ").merge(develop, " ");
+master.commit({messageDisplay:false}).merge(develop, {messageDisplay:false});
 develop.commit({messageDisplay:false});
 
 var feature1 = gitgraph.branch({parentBranch:develop, name:"feature/1", column:featureCol});
@@ -68,35 +74,35 @@ feature2.commit("Another feature to go into v1.0.0").commit({messageDisplay:fals
 feature2.merge(develop);
 
 var release_100 = gitgraph.branch({parentBranch: develop, name: "release/v1.0.0", column:releaseCol});
-release_100.commit({message:"Start v1.0.0-rc Release Candidate builds",marker:"v1.0.0-rc",markerColor:'cyan'});
+release_100.commit({message:"Start v1.0.0-rc Release Candidate builds",tag:"v1.0.0-rc",tagColor:'gray'});
 develop.commit({messageDisplay:false});
 release_100.commit(stablizationCommit);
-release_100.merge(develop).merge(master, {dotStrokeWidth: 10, message: "Release v1.0.0 tagged",marker:"v1.0.0"});
+release_100.merge(develop).merge(master, {dotStrokeWidth: 10, message: "Release v1.0.0 tagged",tag:"v1.0.0"});
 
 var support_10x = gitgraph.branch({parentBranch: master, name: "support/v1.0.x", column:supportCol});
-support_10x.commit({message:"Start v1.0.1-rc Release Candidate builds",marker:"v1.0.1-rc",markerColor:'cyan'}).commit(bugfixCommit);
+support_10x.commit({message:"Start v1.0.1-rc Release Candidate builds",tag:"v1.0.1-rc",tagColor:'gray'}).commit(bugfixCommit);
 
 var feature3 = gitgraph.branch({parentBranch:develop, name:"feature/3", column:featureCol});
 develop.commit({messageDisplay:false});
 feature3.commit("A feature to go into v1.1.0").commit({messageDisplay:false});
 feature3.merge(develop);
 
-support_10x.commit({dotStrokeWidth: 10, message: "Release v1.0.1 tagged",marker:"v1.0.1"}).merge(develop);
+support_10x.commit({dotStrokeWidth: 10, message: "Release v1.0.1 tagged",tag:"v1.0.1"}).merge(develop);
 
 develop.commit({messageDisplay:false});
-support_10x.commit({message:"Start v1.0.2-rc Release Candidate builds",marker:"v1.0.2-rc",markerColor:'cyan'})
-support_10x.commit(bugfixCommit).commit({dotStrokeWidth: 10, message: "Release v1.0.2 tagged",marker:'v1.0.2'});
+support_10x.commit({message:"Start v1.0.2-rc Release Candidate builds",tag:"v1.0.2-rc",tagColor:'gray'})
+support_10x.commit(bugfixCommit).commit({dotStrokeWidth: 10, message: "Release v1.0.2 tagged",tag:'v1.0.2'});
 support_10x.merge(develop);
 develop.commit({messageDisplay:false});
 
 var release_110 = gitgraph.branch({parentBranch: develop, name: "release/v1.1.0", column:releaseCol});
-release_110.commit({message:"Start v1.1.0-rc Release Candidate builds",marker:"v1.1.0-rc",markerColor:'cyan'})
+release_110.commit({message:"Start v1.1.0-rc Release Candidate builds",tag:"v1.1.0-rc",tagColor:'gray'})
 release_110.commit(stablizationCommit);
-release_110.merge(develop).merge(master, {dotStrokeWidth: 10, message: "Release v1.1.0 tagged",marker:"v1.1.0"});
+release_110.merge(develop).merge(master, {dotStrokeWidth: 10, message: "Release v1.1.0 tagged",tag:"v1.1.0"});
 
 var support_11x = gitgraph.branch({parentBranch: master, name: "support/v1.1.x", column:supportCol});
-support_11x.commit({message:"Start v1.1.1-rc Release Candidate builds",marker:"v1.1.1-rc",markerColor:'cyan'})
-support_11x.commit(bugfixCommit).commit({dotStrokeWidth: 10, message: "Release v1.1.1 tagged",marker:"v1.1.1"});
+support_11x.commit({message:"Start v1.1.1-rc Release Candidate builds",tag:"v1.1.1-rc",tagColor:'gray'})
+support_11x.commit(bugfixCommit).commit({dotStrokeWidth: 10, message: "Release v1.1.1 tagged",tag:"v1.1.1"});
 support_11x.merge(develop);
 develop.commit({messageDisplay:false});
 
@@ -105,8 +111,8 @@ develop.commit({messageDisplay:false});
 feature4.commit("A feature to go into v1.2.0").commit({messageDisplay:false});
 feature4.merge(develop);
 
-support_11x.commit({message:"Start v1.1.2-rc Release Candidate builds",marker:"v1.1.2-rc",markerColor:'cyan'})
-support_11x.commit(bugfixCommit).commit({dotStrokeWidth: 10, message: "Release v1.1.2",marker:"v1.1.2"});
+support_11x.commit({message:"Start v1.1.2-rc Release Candidate builds",tag:"v1.1.2-rc",tagColor:'gray'})
+support_11x.commit(bugfixCommit).commit({dotStrokeWidth: 10, message: "Release v1.1.2",tag:"v1.1.2"});
 support_11x.merge(develop);
 develop.commit({messageDisplay:false});
 
@@ -115,13 +121,13 @@ develop.commit({messageDisplay:false});
 feature5.commit("Another feature to go into v1.2.0").commit({messageDisplay:false});
 feature5.merge(develop);
 
-support_11x.commit({message:"Start v1.1.3-rc Release Candidate builds",marker:"v1.1.3-rc",markerColor:'cyan'})
-support_11x.commit(bugfixCommit).commit({dotStrokeWidth: 10, message: "Release v1.1.3 tagged",marker:"v1.1.3"});
+support_11x.commit({message:"Start v1.1.3-rc Release Candidate builds",tag:"v1.1.3-rc",tagColor:'gray'})
+support_11x.commit(bugfixCommit).commit({dotStrokeWidth: 10, message: "Release v1.1.3 tagged",tag:"v1.1.3"});
 support_11x.merge(develop);
 develop.commit({messageDisplay:false});
 
 var release_120 = gitgraph.branch({parentBranch: develop, name: "release/v1.2.0", column:releaseCol});
-release_120.commit({message:"Start v1.2.0-rc Release Candidate builds",marker:"v1.2.0-rc",markerColor:'cyan'})
+release_120.commit({message:"Start v1.2.0-rc Release Candidate builds",tag:"v1.2.0-rc",tagColor:'gray'})
 release_120.commit(stablizationCommit);
-release_120.merge(develop).merge(master, {dotStrokeWidth: 10, message: "Release v1.2.0 tagged",marker:"v1.2.0"});
+release_120.merge(develop).merge(master, {dotStrokeWidth: 10, message: "Release v1.2.0 tagged",tag:"v1.2.0"});
 develop.commit({messageDisplay:false});
