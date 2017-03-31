5092ee528708cbaebba1d973a9927e65395093ae
fabien0102 <fabien0102@planee.fr>
2017-03-31 15:14:45 +0200

    Remove ts example (should not be commit…)

--- examples/index.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="../dist/gitgraph.d.ts" />
-
-const g:GitGraph = new GitGraph({orientation: 'vertical'});
-
-const template:GitGraph.Template = new GitGraph.Template({});
-
-g.commit("plop");
