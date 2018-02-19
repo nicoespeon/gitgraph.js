05b2604fa5117ea5ddffbe9353849f48e7d81d22
fabien0102 <fabien0102@planee.fr>
2018-02-19 15:22:37 +0100

    Move tests to branch.test.ts

---
+++ packages/gitgraph-core/src/__tests__/branch.test.ts
@@ -0,0 +1,61 @@
+import GitGraph from "../gitgraph";
+
+describe("Branch", () => {
+
+  class G extends GitGraph { public render(): void { return null; } }
+
+  describe("commit", () => {
+    describe("on HEAD", () => {
+      // tslint:disable-next-line:one-variable-per-declaration
+      let one, two, three;
+
+      beforeEach(() => {
+        const gitgraph = new G();
+
+        gitgraph
+          .commit()
+          .commit()
+          .branch("develop")
+          .commit();
+
+        [one, two, three] = gitgraph.log();
+      });
+
+      it("should keep master tag on the second commit", () => {
+        expect(two.refs).toEqual(["master"]);
+      });
+
+      it("should have develop and head tags on the last commit", () => {
+        expect(three.refs).toEqual(["develop", "HEAD"]);
+      });
+
+      it("should have the correct parents set", () => {
+        expect(three.parents).toEqual([two.commit]);
+      });
+    });
+
+    describe("with variables", () => {
+      let one, two, three, four;
+
+      beforeEach(() => {
+        const gitgraph = new G();
+
+        const master = gitgraph.branch("master");
+        master.commit().commit(); // one, two
+        const dev = gitgraph.branch("develop");
+        dev.commit(); // tree
+        master.commit(); // four
+
+        [one, two, three, four] = gitgraph.log();
+      });
+
+      it("should have master tag on four commit", () => {
+        expect(four.refs).toEqual(["master"]);
+      });
+
+      it("should have develop and head tags on three commit", () => {
+        expect(three.refs).toEqual(["develop", "HEAD"]);
+      });
+    })
+  });
+})
