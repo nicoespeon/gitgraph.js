9014ff6ea8e8a5f54e75338ed66c91f60ce0395d
fabien0102 <fabien0102@planee.fr>
2018-02-28 17:51:38 +0100

    Add a utils.debug function for DX

--- packages/gitgraph-core/src/utils.ts
+++ packages/gitgraph-core/src/utils.ts
@@ -1,3 +1,5 @@
+import { Commit } from "./commit";
+
 /**
  * Provide a default value to a boolean.
  * @param value
@@ -14,4 +16,20 @@ export function booleanOptionOr(value: any, defaultValue: boolean): boolean {
  */
 export function numberOptionOr(value: any, defaultValue: number | null): number | null {
   return typeof value === "number" ? value : defaultValue;
+}
+
+/**
+ * Print a light version of commits into the console.
+ * @param commits List of commits
+ * @param whitelist List of picked keys
+ */
+export function debug(commits: Commit[], whitelist: Array<keyof Commit>): void {
+  // tslint:disable-next-line:no-console
+  console.log(
+    JSON.stringify(commits.map((commit) =>
+      (Object.keys(commit) as Array<keyof Commit>)
+        .filter((key) => whitelist.includes(key))
+        .reduce((mem, key) => ({ ...mem, [key]: commit[key] }), {}))
+      , null, 2),
+  );
 }
