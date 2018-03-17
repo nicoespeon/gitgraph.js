c2851da9803dccaa1436da13ab05d0a03f5278f5
fabien0102 <fabien0102@planee.fr>
2018-03-17 18:33:03 +0100

    Add a playground and deal correctly with the render

--- packages/gitgraph-react/src/GitgraphReact.ts
+++ packages/gitgraph-react/src/GitgraphReact.ts
@@ -1,14 +1,21 @@
-import { GitGraph as GitGraphCore } from "gitgraph-core/lib/index";
+import { Component } from "react";
+import { GitGraph as GitGraphCore, GitGraphOptions } from "gitgraph-core/lib/index";
 
 /**
  * Implementation of gitgraph-core for react
  *
- * It update `state.commits` of the component on each render.
+ * It simply update the state of the component on each render.
  */
 export class GitgraphReact extends GitGraphCore {
+  private component: Component;
+
+  constructor(options: GitGraphOptions, component: Component) {
+    super(options);
+    this.component = component;
+  }
 
   public render(): void {
-    return;
+    return this.component.setState({ commits: this.log() });
   }
 }
 
