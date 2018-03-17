7e3545c25af75addf94cd5e4147f6679edfe3bdd
fabien0102 <fabien0102@planee.fr>
2018-03-17 17:07:40 +0100

    POC of gitgraph-core integration into gitgraph-react
    A lot of hacks for instant, definitively not perfect, but it's a first step!

--- packages/gitgraph-react/src/GitgraphReact.ts
+++ packages/gitgraph-react/src/GitgraphReact.ts
@@ -1,21 +1,14 @@
-import { Component } from "react";
-import GitgraphCore, { GitGraphOptions } from "gitgraph-core";
+import { GitGraph as GitGraphCore } from "gitgraph-core/lib/index";
 
 /**
  * Implementation of gitgraph-core for react
  *
  * It update `state.commits` of the component on each render.
  */
-export class GitgraphReact extends GitgraphCore {
-  private setState: Component["setState"];
-
-  constructor(options: GitGraphOptions, setState: Component["setState"]) {
-    super(options);
-    this.setState = setState;
-  }
+export class GitgraphReact extends GitGraphCore {
 
   public render(): void {
-    return this.setState({ commits: this.log() });
+    return;
   }
 }
 
--- packages/gitgraph-react/src/stories/index.stories.tsx
+++ packages/gitgraph-react/src/stories/index.stories.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { Gitgraph } from "../Gitgraph";
-import { GitgraphReact } from "../GitgraphReact";
 import { storiesOf } from "@storybook/react";
 
 storiesOf("Hello", module)
   .add("default", () => <Gitgraph>
-    {(gitgraph: GitgraphReact) => {
+    {(gitgraph) => {
       gitgraph
         .branch("master")
         .commit("one")
-        .commit("two");
+        .commit("two")
+        .commit("three");
     }}
   </Gitgraph>);
