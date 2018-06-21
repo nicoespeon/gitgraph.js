26c1ab44b497cee402fdd97667f2c96c7d531ec3
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-06-20 22:09:04 -0400

    Add simple branch case

--- packages/gitgraph-cli/src/render.test.ts
+++ packages/gitgraph-cli/src/render.test.ts
@@ -1,12 +1,20 @@
-import { GitgraphCore } from "gitgraph-core/lib/index";
-import render from "./render";
+import { GitgraphCore, Branch } from "gitgraph-core/lib/index";
+import render, { IRenderGraph } from "./render";
 
-it("should log commit message for a single commit", () => {
-  const logger = {
+let gitgraph: GitgraphCore;
+let master: Branch;
+let logger: IRenderGraph;
+
+beforeEach(() => {
+  gitgraph = new GitgraphCore();
+  master = gitgraph.branch("master");
+  logger = {
     commit: jest.fn()
   };
-  const gitgraph = new GitgraphCore();
-  gitgraph.commit({
+});
+
+it("should log commit message for a single commit", () => {
+  master.commit({
     hash: "9a58c0b5939a20a929bf3ade9b2974e91106a83f",
     subject: "Hello"
   });
@@ -18,23 +26,35 @@ it("should log commit message for a single commit", () => {
 });
 
 it("should log commit messages for multiple commits", () => {
-  const logger = {
-    commit: jest.fn()
-  };
-  const gitgraph = new GitgraphCore();
-  gitgraph.commit({
+  master.commit({
     hash: "9a58c0b5939a20a929bf3ade9b2974e91106a83f",
     subject: "Hello"
   });
-  gitgraph.commit({
+  master.commit({
     hash: "8b4581ad6fc5ceca3726e585c2a46a76a4cd3a23",
     subject: "World!"
   });
 
   render(logger, gitgraph.getRenderedData());
 
-  expect(logger.commit.mock.calls).toEqual([
+  const commitFn = (logger.commit as jest.Mock);
+  expect(commitFn.mock.calls).toEqual([
     ["9a58c0b", "Hello"],
     ["8b4581a", "World!"]
+  ]);
+});
+
+it("should log a branch with one commit like it was a single line", () => {
+  master.commit("one");
+  const develop = gitgraph.branch("develop");
+  develop.commit("two");
+
+  render(logger, gitgraph.getRenderedData());
+
+  const commitFn = (logger.commit as jest.Mock);
+  expect(commitFn).toHaveBeenCalledTimes(2);
+  expect(commitFn.mock.calls).toEqual([
+    [expect.any(String), "one"],
+    [expect.any(String), "two"]
   ]);
 });
