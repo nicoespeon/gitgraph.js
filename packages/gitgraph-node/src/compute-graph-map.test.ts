a962ee2e1489937d2765b5b50acd1c2a9f896a7d
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2018-06-27 22:23:23 -0400

    Move all tests into a context

--- packages/gitgraph-node/src/compute-graph-map.test.ts
+++ packages/gitgraph-node/src/compute-graph-map.test.ts
@@ -5,222 +5,224 @@ import computeGraphMap, { GraphCommit, GraphMap } from "./compute-graph-map";
 let gitgraph: GitgraphCore;
 let master: Branch;
 
-function expectGraphMapValues(graphMap: GraphMap) {
-  return {
-    toEqual(expected: any): void {
-      const graphMapValues = graphMap.map((line) =>
-        line.map(({ value }) => value)
-      );
-      return expect(graphMapValues).toEqual(expected);
-    }
-  };
-}
+describe("compute cells values", () => {
+  function expectGraphMapValues(graphMap: GraphMap) {
+    return {
+      toEqual(expected: any): void {
+        const graphMapValues = graphMap.map((line) =>
+          line.map(({ value }) => value)
+        );
+        return expect(graphMapValues).toEqual(expected);
+      }
+    };
+  }
 
-beforeEach(() => {
-  gitgraph = new GitgraphCore();
-  master = gitgraph.branch("master");
-});
-
-it("should log graph for a single commit on a single branch", () => {
-  master.commit({
-    hash: "9a58c0b5939a20a929bf3ade9b2974e91106a83f",
-    subject: "Hello"
+  beforeEach(() => {
+    gitgraph = new GitgraphCore();
+    master = gitgraph.branch("master");
   });
 
-  const graphMap = computeGraphMap(gitgraph);
+  it("for a single commit on a single branch", () => {
+    master.commit({
+      hash: "9a58c0b5939a20a929bf3ade9b2974e91106a83f",
+      subject: "Hello"
+    });
 
-  const graphCommit: GraphCommit = {
-    hash: "9a58c0b",
-    message: "Hello",
-    refs: ["master", "HEAD"]
-  };
-  expectGraphMapValues(graphMap).toEqual([
-    ["*", graphCommit]
-  ]);
-});
+    const graphMap = computeGraphMap(gitgraph);
 
-it("should log graph for multiple commits on a single branch", () => {
-  master.commit({
-    hash: "9a58c0b5939a20a929bf3ade9b2974e91106a83f",
-    subject: "Hello"
-  });
-  master.commit({
-    hash: "8b4581ad6fc5ceca3726e585c2a46a76a4cd3a23",
-    subject: "World!"
+    const graphCommit: GraphCommit = {
+      hash: "9a58c0b",
+      message: "Hello",
+      refs: ["master", "HEAD"]
+    };
+    expectGraphMapValues(graphMap).toEqual([
+      ["*", graphCommit]
+    ]);
   });
 
-  const graphMap = computeGraphMap(gitgraph);
+  it("for multiple commits on a single branch", () => {
+    master.commit({
+      hash: "9a58c0b5939a20a929bf3ade9b2974e91106a83f",
+      subject: "Hello"
+    });
+    master.commit({
+      hash: "8b4581ad6fc5ceca3726e585c2a46a76a4cd3a23",
+      subject: "World!"
+    });
 
-  const graphCommit1: GraphCommit = {
-    hash: "9a58c0b",
-    message: "Hello",
-    refs: []
-  };
-  const graphCommit2: GraphCommit = {
-    hash: "8b4581a",
-    message: "World!",
-    refs: ["master", "HEAD"]
-  };
-  expectGraphMapValues(graphMap).toEqual([
-    ["*", graphCommit1],
-    ["*", graphCommit2]
-  ]);
-});
+    const graphMap = computeGraphMap(gitgraph);
 
-it("should log graph for multiple commits on 2 branches (fast-forward)", () => {
-  master.commit("one");
-  const develop = gitgraph.branch("develop");
-  develop.commit("two");
-
-  const graphMap = computeGraphMap(gitgraph);
-
-  const masterGraphCommit = expect.objectContaining({
-    message: "one",
-    refs: ["master"]
+    const graphCommit1: GraphCommit = {
+      hash: "9a58c0b",
+      message: "Hello",
+      refs: []
+    };
+    const graphCommit2: GraphCommit = {
+      hash: "8b4581a",
+      message: "World!",
+      refs: ["master", "HEAD"]
+    };
+    expectGraphMapValues(graphMap).toEqual([
+      ["*", graphCommit1],
+      ["*", graphCommit2]
+    ]);
   });
-  const developGraphCommit = expect.objectContaining({
-    message: "two",
-    refs: ["develop", "HEAD"]
+
+  it("for multiple commits on 2 branches (fast-forward)", () => {
+    master.commit("one");
+    const develop = gitgraph.branch("develop");
+    develop.commit("two");
+
+    const graphMap = computeGraphMap(gitgraph);
+
+    const masterGraphCommit = expect.objectContaining({
+      message: "one",
+      refs: ["master"]
+    });
+    const developGraphCommit = expect.objectContaining({
+      message: "two",
+      refs: ["develop", "HEAD"]
+    });
+    expectGraphMapValues(graphMap).toEqual([
+      ["*", " ", " ", masterGraphCommit],
+      [" ", "\\", " ", " "],
+      [" ", " ", "*", developGraphCommit]
+    ]);
   });
-  expectGraphMapValues(graphMap).toEqual([
-    ["*", " ", " ", masterGraphCommit],
-    [" ", "\\", " ", " "],
-    [" ", " ", "*", developGraphCommit]
-  ]);
-});
 
-it("should log graph for multiple commits on 2 branches (no fast-forward)", () => {
-  master.commit("one");
-  const develop = gitgraph.branch("develop");
-  develop.commit("two");
-  master.commit("three");
-  develop.commit("four");
+  it("for multiple commits on 2 branches (no fast-forward)", () => {
+    master.commit("one");
+    const develop = gitgraph.branch("develop");
+    develop.commit("two");
+    master.commit("three");
+    develop.commit("four");
 
-  const graphMap = computeGraphMap(gitgraph);
+    const graphMap = computeGraphMap(gitgraph);
 
-  const graphCommit1 = expect.objectContaining({
-    message: "one",
-    refs: []
+    const graphCommit1 = expect.objectContaining({
+      message: "one",
+      refs: []
+    });
+    const graphCommit2 = expect.objectContaining({
+      message: "two",
+      refs: []
+    });
+    const graphCommit3 = expect.objectContaining({
+      message: "three",
+      refs: ["master"]
+    });
+    const graphCommit4 = expect.objectContaining({
+      message: "four",
+      refs: ["develop", "HEAD"]
+    });
+    expectGraphMapValues(graphMap).toEqual([
+      ["*", " ", " ", graphCommit1],
+      ["|", "\\", " ", " "],
+      ["|", " ", "*", graphCommit2],
+      ["*", " ", "|", graphCommit3],
+      [" ", " ", "*", graphCommit4],
+    ]);
   });
-  const graphCommit2 = expect.objectContaining({
-    message: "two",
-    refs: []
+
+  it("for 2 branches with merge", () => {
+    master.commit("one");
+    const develop = gitgraph.branch("develop");
+    develop.commit("two");
+    master.merge(develop);
+
+    const graphMap = computeGraphMap(gitgraph);
+
+    const masterGraphCommit = expect.objectContaining({
+      message: "one",
+      refs: []
+    });
+    const developGraphCommit = expect.objectContaining({
+      message: "two",
+      refs: ["develop"]
+    });
+    const mergeCommit = expect.objectContaining({
+      refs: ["master", "HEAD"]
+    });
+    expectGraphMapValues(graphMap).toEqual([
+      ["*", " ", " ", masterGraphCommit],
+      ["|", "\\", " ", " "],
+      ["|", " ", "*", developGraphCommit],
+      ["|", "/", " ", " "],
+      ["*", " ", " ", mergeCommit],
+    ]);
   });
-  const graphCommit3 = expect.objectContaining({
-    message: "three",
-    refs: ["master"]
+
+  it("for 3 branches (consecutive)", () => {
+    master.commit("one");
+
+    const feat1 = gitgraph.branch("feat1");
+    feat1.commit("two");
+
+    const feat2 = gitgraph.branch("feat2");
+    feat2.commit("three");
+
+    const graphMap = computeGraphMap(gitgraph);
+
+    const masterCommit = expect.objectContaining({
+      message: "one",
+      refs: ["master"]
+    });
+    const feat1Commit = expect.objectContaining({
+      message: "two",
+      refs: ["feat1"]
+    });
+    const feat2Commit = expect.objectContaining({
+      message: "three",
+      refs: ["feat2", "HEAD"]
+    });
+    expectGraphMapValues(graphMap).toEqual([
+      ["*", " ", " ", " ", " ", masterCommit],
+      [" ", "\\", " ", " ", " ", " "],
+      [" ", " ", "*", " ", " ", feat1Commit],
+      [" ", " ", " ", "\\", " ", " "],
+      [" ", " ", " ", " ", "*", feat2Commit],
+    ]);
   });
-  const graphCommit4 = expect.objectContaining({
-    message: "four",
-    refs: ["develop", "HEAD"]
+
+  it("for 3 branches (from master)", () => {
+    master.commit("one");
+
+    const feat1 = gitgraph.branch("feat1");
+    feat1.commit("two");
+
+    // Reset HEAD to master
+    master.commit("three");
+
+    const feat2 = gitgraph.branch("feat2");
+    feat2.commit("four");
+
+    const graphMap = computeGraphMap(gitgraph);
+
+    const masterCommit1 = expect.objectContaining({
+      message: "one",
+      refs: []
+    });
+    const feat1Commit = expect.objectContaining({
+      message: "two",
+      refs: ["feat1"]
+    });
+    const masterCommit2 = expect.objectContaining({
+      message: "three",
+      refs: ["master"]
+    });
+    const feat2Commit = expect.objectContaining({
+      message: "four",
+      refs: ["feat2", "HEAD"]
+    });
+    expectGraphMapValues(graphMap).toEqual([
+      ["*", " ", " ", " ", " ", masterCommit1],
+      ["|", "\\", " ", " ", " ", " "],
+      ["|", " ", "*", " ", " ", feat1Commit],
+      ["*", " ", " ", " ", " ", masterCommit2],
+      [" ", "\\", " ", " ", " ", " "],
+      [" ", " ", "\\", " ", " ", " "],
+      [" ", " ", " ", "\\", " ", " "],
+      [" ", " ", " ", " ", "*", feat2Commit],
+    ]);
   });
-  expectGraphMapValues(graphMap).toEqual([
-    ["*", " ", " ", graphCommit1],
-    ["|", "\\", " ", " "],
-    ["|", " ", "*", graphCommit2],
-    ["*", " ", "|", graphCommit3],
-    [" ", " ", "*", graphCommit4],
-  ]);
-});
-
-it("should log graph for 2 branches with merge", () => {
-  master.commit("one");
-  const develop = gitgraph.branch("develop");
-  develop.commit("two");
-  master.merge(develop);
-
-  const graphMap = computeGraphMap(gitgraph);
-
-  const masterGraphCommit = expect.objectContaining({
-    message: "one",
-    refs: []
-  });
-  const developGraphCommit = expect.objectContaining({
-    message: "two",
-    refs: ["develop"]
-  });
-  const mergeCommit = expect.objectContaining({
-    refs: ["master", "HEAD"]
-  });
-  expectGraphMapValues(graphMap).toEqual([
-    ["*", " ", " ", masterGraphCommit],
-    ["|", "\\", " ", " "],
-    ["|", " ", "*", developGraphCommit],
-    ["|", "/", " ", " "],
-    ["*", " ", " ", mergeCommit],
-  ]);
-});
-
-it("should log graph for 3 branches (consecutive)", () => {
-  master.commit("one");
-
-  const feat1 = gitgraph.branch("feat1");
-  feat1.commit("two");
-
-  const feat2 = gitgraph.branch("feat2");
-  feat2.commit("three");
-
-  const graphMap = computeGraphMap(gitgraph);
-
-  const masterCommit = expect.objectContaining({
-    message: "one",
-    refs: ["master"]
-  });
-  const feat1Commit = expect.objectContaining({
-    message: "two",
-    refs: ["feat1"]
-  });
-  const feat2Commit = expect.objectContaining({
-    message: "three",
-    refs: ["feat2", "HEAD"]
-  });
-  expectGraphMapValues(graphMap).toEqual([
-    ["*", " ", " ", " ", " ", masterCommit],
-    [" ", "\\", " ", " ", " ", " "],
-    [" ", " ", "*", " ", " ", feat1Commit],
-    [" ", " ", " ", "\\", " ", " "],
-    [" ", " ", " ", " ", "*", feat2Commit],
-  ]);
-});
-
-it("should log graph for 3 branches (from master)", () => {
-  master.commit("one");
-
-  const feat1 = gitgraph.branch("feat1");
-  feat1.commit("two");
-
-  // Reset HEAD to master
-  master.commit("three");
-
-  const feat2 = gitgraph.branch("feat2");
-  feat2.commit("four");
-
-  const graphMap = computeGraphMap(gitgraph);
-
-  const masterCommit1 = expect.objectContaining({
-    message: "one",
-    refs: []
-  });
-  const feat1Commit = expect.objectContaining({
-    message: "two",
-    refs: ["feat1"]
-  });
-  const masterCommit2 = expect.objectContaining({
-    message: "three",
-    refs: ["master"]
-  });
-  const feat2Commit = expect.objectContaining({
-    message: "four",
-    refs: ["feat2", "HEAD"]
-  });
-  expectGraphMapValues(graphMap).toEqual([
-    ["*", " ", " ", " ", " ", masterCommit1],
-    ["|", "\\", " ", " ", " ", " "],
-    ["|", " ", "*", " ", " ", feat1Commit],
-    ["*", " ", " ", " ", " ", masterCommit2],
-    [" ", "\\", " ", " ", " ", " "],
-    [" ", " ", "\\", " ", " ", " "],
-    [" ", " ", " ", "\\", " ", " "],
-    [" ", " ", " ", " ", "*", feat2Commit],
-  ]);
 });
