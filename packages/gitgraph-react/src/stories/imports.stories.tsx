e093498f81c708c6ec6c756e1a6cdf5c9ddfc1e9
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-01-05 06:58:28 -0500

    Remove old TODO

--- packages/gitgraph-react/src/stories/imports.stories.tsx
+++ packages/gitgraph-react/src/stories/imports.stories.tsx
@@ -16,9 +16,6 @@ storiesOf("Gitgraph imports", module)
   .add("deleted branch", () => (
     <Gitgraph>
       {(gitgraph) => {
-        // TODO: fix this scenario
-        // Commit e90efc7 has no branch because it was merged and deleted.
-        // Since branch was merged, it should be part of the host branch.
         gitgraph.import(deletedBranch);
       }}
     </Gitgraph>
