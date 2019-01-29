02e5eda53f23235dcc98fa55f506a1df0d180b45
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-01-29 18:45:52 -0500

    Update examples & gitgraph.min.js version

--- examples/index.js
+++ examples/index.js
@@ -6,12 +6,16 @@ var myTemplateConfig = {
   colors: ["#F00", "#0F0", "#00F"], // branches colors, 1 per column
   branch: {
     lineWidth: 8,
+    // Dash segments, see:
+    // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setLineDash
+    lineDash: [5, 3],
     spacingX: 50
   },
   commit: {
     spacingY: -80,
     dot: {
-      size: 12
+      size: 12,
+      lineDash: [4]
     },
     message: {
       displayAuthor: true,
@@ -20,7 +24,7 @@ var myTemplateConfig = {
       font: "normal 12pt Arial"
     },
     shouldDisplayTooltipsInCompactMode: false, // default = true
-    tooltipHTMLFormatter: function (commit) {
+    tooltipHTMLFormatter: function(commit) {
       return "<b>" + commit.sha1 + "</b>" + ": " + commit.message;
     }
   }
@@ -32,10 +36,10 @@ var myTemplate = new GitGraph.Template(myTemplateConfig);
  ***********************/
 
 var config = {
-  template: "metro" // could be: "blackarrow" or "metro" or `myTemplate` (custom Template object)
-    //, reverseArrow: true  // to make arrows point to ancestors, if displayed
-    //, orientation: "vertical-reverse"
-    //, mode: "compact"     // special compact mode: hide messages & compact graph
+  template: "blackarrow", // could be: "blackarrow" or "metro" or `myTemplate` (custom Template object)
+  reverseArrow: false, // to make arrows point to ancestors, if displayed
+  orientation: "vertical"
+  // mode: "compact" // special compact mode: hide messages & compact graph
 };
 var gitGraph = new GitGraph(config);
 
@@ -53,9 +57,10 @@ gitGraph.commit("Initial commit");
 gitGraph.commit("My second commit").commit("Add awesome feature");
 
 // Create a new "dev" branch from "master" with some custom configuration
-var dev = gitGraph.branch({
+var dev = master.branch({
   name: "dev",
   color: "#F00",
+  // lineDash: [5],
   commitDefaultOptions: {
     color: "#F00"
   }
@@ -72,11 +77,16 @@ var commitConfig = {
   dotStrokeWidth: 10,
   messageHashDisplay: false,
   messageAuthorDisplay: true,
+  commitDotText: "C1",
   message: "Alors c'est qui le papa ?",
   tooltipDisplay: false,
   author: "Me <me@planee.fr>"
 };
 gitGraph.commit(commitConfig);
+
+// Create another from "master"
+var feature3 = master.branch("feature3");
+feature3.commit().commit();
 
 /***********************
  *      CHECKOUT       *
@@ -90,8 +100,8 @@ gitGraph.commit(commitConfig);
  ***********************/
 
 var commitWithDetailsConfig = {
-  message: "test",
-  detailId: "detail" // Id of detail div (available in normal vertical mode only)
+  message: "A commit with detailed message",
+  detailId: "detail"
 };
 gitGraph.commit(commitWithDetailsConfig).commit();
 dev.commit().commit(); // 2 default commits on "dev"
@@ -119,7 +129,8 @@ test.merge(master, "My special merge commit message");
 
 // Then, continue committing on the "test" branch
 test.commit({
-  message: "It works !"
+  message: "It works !",
+  commitDotText: "C2"
 });
 
 var fastForwardBranch = test.branch("fast-forward");
@@ -167,20 +178,35 @@ test.merge(master, {
   tag: "v1.0.0"
 });
 
+// Create different branches from an empty one and do some commits
+var features = master.branch("features");
+var feature1 = features.branch("feature1");
+var feature2 = features.branch("feature2");
+feature2.commit().commit();
+feature1.commit();
+
 /***********************
  *       EVENTS        *
  ***********************/
 
-gitGraph.canvas.addEventListener("graph:render", function (event) {
-  console.log(event.data.id, "has been rendered with a scaling factor of", gitGraph.scalingFactor);
+gitGraph.canvas.addEventListener("graph:render", function(event) {
+  console.log(
+    event.data.id,
+    "has been rendered with a scaling factor of",
+    gitGraph.scalingFactor
+  );
 });
 
-gitGraph.canvas.addEventListener("commit:mouseover", function (event) {
-  console.log("You're over a commit.", "Here is a bunch of data ->", event.data);
+gitGraph.canvas.addEventListener("commit:mouseover", function(event) {
+  console.log(
+    "You're over a commit.",
+    "Here is a bunch of data ->",
+    event.data
+  );
   this.style.cursor = "pointer";
 });
 
-gitGraph.canvas.addEventListener("commit:mouseout", function (event) {
+gitGraph.canvas.addEventListener("commit:mouseout", function(event) {
   console.log("You just left this commit ->", event.data);
   this.style.cursor = "auto";
 });
@@ -189,7 +215,19 @@ gitGraph.canvas.addEventListener("commit:mouseout", function (event) {
 test.commit({
   message: "Click me!",
   author: "Nicolas <me@planee.fr>",
-  onClick: function (commit, isOverCommit, event) {
+  onClick: function(commit, isOverCommit, event) {
     console.log("You just clicked my commit.", commit, event);
   }
+});
+
+// Display WIP-like commit
+test.commit({
+  lineDash: [3, 2],
+  dotStrokeWidth: 5,
+  dotColor: "white",
+  messageHashDisplay: false,
+  messageAuthorDisplay: false,
+  message: "Current WIP",
+  tag: "HEAD",
+  displayTagBox: false
 });
