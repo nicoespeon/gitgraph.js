9551c248b96d8bead3eb0069988ab7dc428bc8b5
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-03-05 07:02:20 -0500

    Add tests over graph orientation

---
+++ packages/gitgraph-core/src/__tests__/gitgraph.orientation.test.ts
@@ -0,0 +1,34 @@
+import { GitgraphCore } from "../gitgraph";
+import { Orientation } from "../orientation";
+
+describe("Gitgraph Orientation", () => {
+  it("should tell it's vertical by default", () => {
+    const gitgraph = new GitgraphCore();
+
+    expect(gitgraph.isVertical).toBe(true);
+  });
+
+  it("should tell it's vertical in VerticalReverse orientation", () => {
+    const gitgraph = new GitgraphCore({
+      orientation: Orientation.VerticalReverse,
+    });
+
+    expect(gitgraph.isVertical).toBe(true);
+  });
+
+  it("should tell it's horizontal in Horizontal orientation", () => {
+    const gitgraph = new GitgraphCore({
+      orientation: Orientation.Horizontal,
+    });
+
+    expect(gitgraph.isVertical).toBe(false);
+  });
+
+  it("should tell it's horizontal in HorizontalReverse orientation", () => {
+    const gitgraph = new GitgraphCore({
+      orientation: Orientation.HorizontalReverse,
+    });
+
+    expect(gitgraph.isVertical).toBe(false);
+  });
+});
