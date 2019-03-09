0553fffa67362fc1f7cffe75945a4fef50975923
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-03-08 20:25:54 -0500

    Configure Storybook

--- packages/gitgraph-react/.storybook/config.js
+++ packages/gitgraph-react/.storybook/config.js
@@ -1,4 +1,13 @@
-import { configure } from "@storybook/react";
+import { addParameters, configure } from "@storybook/react";
+
+addParameters({
+  options: {
+    name: "@gitgraph/react",
+    url:
+      "https://github.com/nicoespeon/gitgraph.js/tree/master/packages/gitgraph-react",
+    hierarchySeparator: null,
+  },
+});
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context("../src/stories", true, /.stories.tsx$/);
