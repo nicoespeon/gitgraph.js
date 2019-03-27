ebad2d2de02b8fe8bfb41787146990f14e230691
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-03-26 22:17:47 -0400

    Refactor commit message logic in core

--- packages/gitgraph-core/src/__tests__/commit.test.ts
+++ packages/gitgraph-core/src/__tests__/commit.test.ts
@@ -16,4 +16,47 @@ describe("Commit", () => {
     expect(commit.style.dot.color).not.toBe("green");
     expect(style.dot.color).not.toBe("green");
   });
+
+  it("should compute message from commit information", () => {
+    const commit = new Commit({
+      author: "Nicolas Carlo <nicolas.carlo@gmail.com>",
+      hash: "1234567890",
+      subject: "A new commit",
+      style: blackArrowTemplate.commit,
+    });
+
+    expect(commit.message).toBe(
+      "1234567 A new commit - Nicolas Carlo <nicolas.carlo@gmail.com>",
+    );
+  });
+
+  it("should not include commit hash in message if configured in style", () => {
+    const commit = new Commit({
+      author: "Nicolas Carlo <nicolas.carlo@gmail.com>",
+      hash: "1234567890",
+      subject: "A new commit",
+      style: {
+        ...blackArrowTemplate.commit,
+        message: { ...blackArrowTemplate.commit.message, displayHash: false },
+      },
+    });
+
+    expect(commit.message).toBe(
+      "A new commit - Nicolas Carlo <nicolas.carlo@gmail.com>",
+    );
+  });
+
+  it("should not include commit author in message if configured in style", () => {
+    const commit = new Commit({
+      author: "Nicolas Carlo <nicolas.carlo@gmail.com>",
+      hash: "1234567890",
+      subject: "A new commit",
+      style: {
+        ...blackArrowTemplate.commit,
+        message: { ...blackArrowTemplate.commit.message, displayAuthor: false },
+      },
+    });
+
+    expect(commit.message).toBe("1234567 A new commit");
+  });
 });
