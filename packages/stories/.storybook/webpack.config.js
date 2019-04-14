250c64e8192b41905cf5423910740c4913108a57
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2019-04-14 14:08:03 -0400

    Add story addon to Storybook

--- packages/stories/.storybook/webpack.config.js
+++ packages/stories/.storybook/webpack.config.js
@@ -4,7 +4,13 @@ module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     include: path.resolve(__dirname, "../src"),
-    loader: require.resolve("ts-loader"),
+    loaders: [
+      {
+        loader: require.resolve("@storybook/addon-storysource/loader"),
+        options: { parser: "typescript" },
+      },
+      require.resolve("ts-loader"),
+    ],
   });
 
   config.resolve.extensions.push(".ts", ".tsx");
