28997672c74b06dd59b31de230e09674a4f9d392
marek <marek.narozniak@gmail.com>
2019-09-21 17:41:39 +0800

    test cases draft for Layout

--- packages/gitgraph-core/src/__tests__/gitgraph.layout.tests.ts
+++ packages/gitgraph-core/src/__tests__/gitgraph.layout.tests.ts
@@ -7,4 +7,22 @@ describe("Gitgraph Layout", () => {
 
     expect(gitgraph.getLayout).toBe(Layout.Regular);
   });
+
+  it("should not re-use any columns for regular layout", () => {
+    const core = new GitgraphCore({
+      layout: Layout.Regular,
+    });
+  });
+
+  it("should re-use closed branches columns for compact layout", () => {
+    const core = new GitgraphCore({
+      layout: Layout.Compact,
+    });
+  });
+
+  it("should re-use all columns for compact layout", () => {
+    const core = new GitgraphCore({
+      layout: Layout.Condensed,
+    });
+  });
 });
