41e8315eb6414aad85f85437d098affb934f0819
marek <marek.narozniak@gmail.com>
2020-06-11 22:05:08 +0800

    script changed it to const...

--- packages/gitgraph-core/src/graph-rows/gitamine.ts
+++ packages/gitgraph-core/src/graph-rows/gitamine.ts
@@ -5,7 +5,7 @@ import { RegularGraphRows } from "./regular";
 export class GitamineGraphRows<TNode> extends RegularGraphRows<TNode> {
   protected computeRowsFromCommits(commits: Array<Commit<TNode>>): void {
     commits.forEach((commit, i) => {
-      let newRow = i;
+      const newRow = i;
 
       // TODO update newRow variable to follow Gitamine algorithm
 
